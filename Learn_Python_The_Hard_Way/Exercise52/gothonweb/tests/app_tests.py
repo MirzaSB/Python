35df91514cc36568b15bd0b8349c5530bf4c6e8b
Mirza <mirzabaig@5TS502117968ML.local>
2015-08-14 23:11:27 -0400

    Completed study drills of Exercise 52

--- Learn_Python_The_Hard_Way/Exercise52/gothonweb/tests/app_tests.py
+++ Learn_Python_The_Hard_Way/Exercise52/gothonweb/tests/app_tests.py
@@ -1,21 +1,37 @@
 from nose.tools import *
 from bin.app import app
 from tests.tools import assert_response
+import unittest
 
 def test_index():
 	# Check that we got a 404 in the / URL.
 	resp = app.request("/")
-	assert_response(resp, status="404")
+	assert_response(resp, status="303 See Other")
+
+def test_paths():
 
 	# Test our first GET request to /hello.
-	resp = app.request("/hello")
-	assert_response(resp)
+	params = "tell+a+joke"
+	resp = app.request("/game", method="POST", data=params)
+	verifyResponseCodeAndSessionCookie(resp, "200 OK", "None")
 
+	params = "action=0131"
+	resp = app.request("/game", method="POST", data=params)
+	verifyResponseCodeAndSessionCookie(resp, "200 OK", "None")
+	
 	# Make sure that the default values work for the form.
 	resp = app.request("/hello", method="POST")
-	assert_response(resp, contains="Nobody")
+	#assert_response(resp, contains="Nobody")
+	verifyResponseCodeAndSessionCookie(resp, "404 Not Found", "not found")
 
 	# Test that we get expected values.
 	data = {'name': 'Zed', 'greet': 'Hola'}
 	resp = app.request("/hello", method="POST", data=data)
-	assert_response(resp, contains="Zed")
+	#assert_response(resp, contains="Zed")
+	verifyResponseCodeAndSessionCookie(resp, "404 Not Found", "not found")
+
+def verifyResponseCodeAndSessionCookie(response, expected_response_code, response_data):
+	print response
+	assert_response(response, status=expected_response_code)
+	assert(response.data == response_data)
+	assert("webpy_session_id=" in response.headers['Set-Cookie'])
